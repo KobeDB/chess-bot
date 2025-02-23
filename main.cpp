025fe92abfe8db7b35186b5a8b3db59aaa750dc3
KobeDB <kobe.debroeck1@gmail.com>
2025-02-23 16:04:59 +0100

    Fix pawn capture bug + handle promotion in pawn move

--- main.cpp
+++ main.cpp
@@ -20,6 +20,9 @@ struct Move {
     int dest_r;
     int dest_c;
     int taken_piece = -1;
+    bool is_promotion = false; // if true, piece refers to the pawn, dest_r & dest_c to the promotion location, promoted_type to the promoted piece
+    Piece_Type promotion_type;
+    bool is_castling = false; // if true, piece refers to the king
 };
 
 struct Piece {
@@ -94,38 +97,64 @@ struct Chess {
             switch (piece.type) {
                 
                 case PAWN: {
+                    Array<Move> raw_moves {};
+
                     // normal forward step
                     int steps_forward = (piece.r == 1 && piece.color == WHITE || piece.r == 6 && piece.color == BLACK) ? 2 : 1;
                     int step_dir = piece.color == WHITE ? 1 : -1;
                     for (int step = 0; step < steps_forward; ++step) {
                         int next_row = piece.r + (step+1) * step_dir;
                         if (board[to_index(next_row, piece.c)]) break; // break to prevent pawn teleporting through blocking pieces
+
                         Move move {};
                         move.piece = i;
                         move.dest_r = next_row;
                         move.dest_c = piece.c;
-                        if (!does_move_cause_self_check(move))
-                            result.push(move);
-                        // TODO: add pawn promotion here
+
+                        raw_moves.push(move);
                     }
 
-                    // taking
-                    for (int attack_c = -1; attack_c <= 1; ++attack_c) {
-                        if (attack_c == 0) continue;
+                    // capturing
+                    for (int attack_c_off = -1; attack_c_off <= 1; ++attack_c_off) {
+                        if (attack_c_off == 0) continue;
+
+                        int attack_c = piece.c + attack_c_off;
                         int attack_r = piece.r + step_dir;
 
                         if (!is_valid_pos(attack_r, attack_c)) continue;
                         
                         const Piece *attacked_piece = board[to_index(attack_r, attack_c)];
                         
-                        if (attacked_piece) {
+                        if (attacked_piece && attacked_piece->color != piece.color) {
                             Move move {};
                             move.piece = i;
                             move.dest_r = attack_r;
                             move.dest_c = attack_c;
                             move.taken_piece = attacked_piece->index;
-                            if (!does_move_cause_self_check(move))
-                                result.push(move);
+
+                            raw_moves.push(move);
+                        }
+                    }
+
+                    // check for promotions
+                    for (int raw_move_i = 0; raw_move_i < raw_moves.size(); ++raw_move_i) {
+                        Move &raw_move = raw_moves[raw_move_i];
+                        if (raw_move.dest_r == 7 && piece.color == WHITE || raw_move.dest_r == 0 && piece.color == BLACK) {
+                            // The pawn must promote => push only promotion moves in result
+
+                            raw_move.is_promotion = true;
+                            
+                            raw_move.promotion_type = QUEEN;
+                            result.push(raw_move);
+                            raw_move.promotion_type = ROOK;
+                            result.push(raw_move);
+                            raw_move.promotion_type = KNIGHT;
+                            result.push(raw_move);
+                            raw_move.promotion_type = BISHOP;
+                            result.push(raw_move);
+                            
+                        } else {
+                            result.push(raw_move);
                         }
                     }
                 } break;
@@ -137,6 +166,17 @@ struct Chess {
         return result;
     }
 
+    void get_taken_pieces(int *taken_pieces, int &taken_pieces_count, int color) const {
+        taken_pieces_count = 0;
+        for (int i = 0; i <32; ++i) {
+            const Piece &piece = pieces[i];
+            if (piece.taken && piece.color == color) {
+                taken_pieces[taken_pieces_count] = i;
+                ++taken_pieces_count;
+            }
+        }
+    }
+
     Chess next_state(const Move &move) const {
         Chess next = *this;
 
@@ -144,6 +184,7 @@ struct Chess {
 
         next.pieces[move.piece].r = move.dest_r;
         next.pieces[move.piece].c = move.dest_c;
+        next.pieces[move.piece].has_moved = true;
 
         if (move.taken_piece != -1) {
             next.pieces[move.taken_piece].taken = true;
@@ -225,8 +266,46 @@ void print_legal_moves(const Chess &chess, const Array<Move> &legal_moves) {
         const Move &move = legal_moves[i];
         const Piece &piece = chess.pieces[move.piece];
         char pc = chess.piece_to_char(piece);
-        printf("%c to (%d, %d)\n", pc, move.dest_r, move.dest_c);
+        printf("%c(%d) to (%d, %d)\n", pc, piece.index, move.dest_r, move.dest_c);
     }
+}
+
+Move get_user_move(Chess &chess, bool &move_ok) {
+    auto legal_moves = chess.legal_moves();
+    print_legal_moves(chess, legal_moves);
+    printf("Give a move: ");
+    char piece {};
+    char col {};
+    int row {};
+    scanf(" %c%c%d", &piece, &col, &row);
+
+    if (col < 'a' || col > 'h') {
+        fprintf(stderr, "col: %c out of bounds\n", col);
+        move_ok = false;
+        return {};
+    }
+    if (row < 1 || row > 8) {
+        fprintf(stderr, "row: %d out of bounds\n", row);
+        move_ok = false;
+        return {};
+    }
+
+    int c = (int)(col - 'a');
+    int r = row - 1;
+
+    printf("user moved: %c to (%d, %d)\n", piece, r, c);
+
+    // find user's move in list of legal moves
+    for (int i = 0; i < legal_moves.size(); ++i) {
+        const Move &move = legal_moves[i];
+        if (move.dest_r == r && move.dest_c == c && chess.piece_to_char(chess.pieces[move.piece]) == piece) {
+            move_ok = true;
+            return move;
+        }
+    }
+
+    move_ok = false;
+    return {};
 }
 
 int main() {
@@ -235,11 +314,21 @@ int main() {
     chess.draw();
     print_legal_moves(chess, chess.legal_moves());
 
+    while (true) {
+        bool user_move_ok = false;
+        while (!user_move_ok) {
+            Move user_move = get_user_move(chess, user_move_ok);
+            if (!user_move_ok) printf("That's an illegal move. Try Again...\n");
+            else chess = chess.next_state(user_move);
+        }
+        chess.draw();
+    }
+
 
     Move move {};
     move.piece = 0;
     move.dest_c = 0;
-    move.dest_r = 3;
+    move.dest_r = 5;
     chess = chess.next_state(move);
     chess.draw();
 
